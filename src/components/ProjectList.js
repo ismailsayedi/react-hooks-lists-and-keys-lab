d96f248699dd7a13ebb84ebfd8b742c4fde7355e
+ <+>
2023-05-27 18:36:49 -0600

    done

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -5,10 +5,19 @@ function ProjectList({ projects }) {
   console.log(projects);
   return (
     <div id="projects">
-      <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+    <h2>My Projects</h2>
+    <div id="project-list">
+      {projects.map((project) => (
+        <ProjectItem
+          key={project.id}
+          name={project.name}
+          about={project.about}
+          technologies={project.technologies}
+        />
+      ))}
     </div>
-  );
+  </div>
+);
 }
 
 export default ProjectList;
